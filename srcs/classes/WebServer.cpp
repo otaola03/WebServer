a163b5896708bda467a73a239bd6a6757f0df7a9
Unai Martin marante <umartin-@c3r4s5.42urduliz.com>
2023-10-17 18:44:29 +0200

    umartin branch 3

--- srcs/classes/WebServer.cpp
+++ srcs/classes/WebServer.cpp
@@ -47,12 +47,14 @@ void	WebServer::serverLoop()
 {
 	fd_set	read_fds;
 	char remoteIP[INET6_ADDRSTRLEN];
-	char buf[256];
+	char buf[100000];
 	int newfd;
 	struct sockaddr_storage remoteaddr;
 	int nbytes;
 	socklen_t addrlen;
 	int fdmax = serversList[0]->fdMax;
+	char msg[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+
 
 	std::cout << "----> " << fdmax << "\n";
 	// main loop
@@ -64,23 +66,23 @@ void	WebServer::serverLoop()
             exit(4);
         }
 
-        // run through the existing connections looking for data to read
-        for(int i = 0; i <= fdmax; i++) {
-            if (FD_ISSET(i, &read_fds)) { // we got one!!
-                if (FD_ISSET(i, &portsList)) {
-                    // handle new connections
+        for(int i = 0; i <= fdmax; i++)
+		{
+            if (FD_ISSET(i, &read_fds)) //Get a connection
+			{
+				std::cout << "ENTRA\n";
+                if (FD_ISSET(i, &portsList)) //New Connection
+				{
                     addrlen = sizeof remoteaddr;
-                    newfd = accept(i,
-                        (struct sockaddr *)&remoteaddr,
-                        &addrlen);
+                    newfd = accept(i, (struct sockaddr *)&remoteaddr, &addrlen);
 
-                    if (newfd == -1) {
+                    if (newfd == -1)
                         perror("accept");
-                    } else {
+                    else
+					{
                         FD_SET(newfd, &socketList); // add to socketList set
-                        if (newfd > fdmax) {    // keep track of the max
+                        if (newfd > fdmax)    // keep track of the max
                             fdmax = newfd;
-                        }
                         printf("selectserver: new connection from %s on "
                             "socket %d\n",
                             inet_ntop(remoteaddr.ss_family,
@@ -88,36 +90,36 @@ void	WebServer::serverLoop()
                                 remoteIP, INET6_ADDRSTRLEN),
                             newfd);
                     }
-                } else {
-                    // handle data from a client
-                    if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+                }
+				
+				else // Connection fron an actual client
+				{
+					std::cout << "------> RCV\n";
+                    if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0)
+					{
                         // got error or connection closed by client
-                        if (nbytes == 0) {
-                            // connection closed
+                        if (nbytes == 0)
                             printf("selectserver: socket %d hung up\n", i);
-                        } else {
+						else
                             perror("recv");
-                        }
                         close(i); // bye!
                         FD_CLR(i, &socketList); // remove from socketList set
-                    } else {
-                        // we got some data from a client
-                        for(int j = 0; j <= fdmax; j++) {
-                            // send to everyone!
-                            if (FD_ISSET(j, &socketList)) {
-                                // except the listener and ourselves
-                                if (!FD_ISSET(j, &portsList) && j != i) {
-                                    if (send(j, buf, nbytes, 0) == -1) {
-                                        perror("send");
-                                    }
-                                }
-                            }
-                        }
+                    }
+
+					else // Data recived
+					{
+						buf[nbytes] = '\0';
+						std::cout << buf << "\n";
+						if (send(i, msg, sizeof(msg), 0) == -1)
+						{
+							perror("send");
+						}
                     }
                 } // END handle data from client
             } // END got new incoming connection
     	} // END looping through file descriptors
 	} // END for(;;)--and you thought it would never end!
+	std::cout << "------------------------------------------------\n";
 }
 
 Server&	WebServer::getServer(const int fd)
