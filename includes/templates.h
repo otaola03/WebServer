984b2bd3e261382f6d9fd3d00f8dbc25bd67208b
Jon Perez <jperez@c3r4s3.42urduliz.com>
2023-10-17 17:31:20 +0200

    created templates.h

---
+++ includes/templates.h
@@ -0,0 +1,11 @@
+#ifndef TEMPLATES_H
+
+#define TEMPLATES_H
+
+typedef std::vector<int> intVector;
+typedef std::vector<Port> portsVector;
+typedef std::vector<Location> locationVector;
+typedef std::map<int, Port*> intPortMap;
+typedef std::map<int, std::string> intCharMap;
+
+#endif
