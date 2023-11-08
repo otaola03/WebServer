a6164f74e49c0d831ff2781f19663995b43e4730
Xabier Matute Rusinol <xmatute-@c3r2s2.42urduliz.com>
2023-11-08 17:12:16 +0100

    ahi tienes location

--- srcs/classes/Location.cpp
+++ srcs/classes/Location.cpp
@@ -6,21 +6,47 @@ Location::Location() : GET(false), POST(false), DELETE(false), autoindex(false)
 		std::cout << "HOLA\n";
 }
 
-Location::Location(
-	const std::string	path, \
-	const std::string	root, \
-	const bool			GET, \
-	const bool			POST, \
-	const bool			DELETE, \
-	const bool			autoindex, \
-	std::string			redirection, \
-	std::string			destination 
-) : path(path), root(root), GET(GET), POST(POST), DELETE(DELETE), autoindex(autoindex), redirection(redirection), destination(destination)
-{
-	if (GET && POST && DELETE && autoindex)
-		std::cout << "HOLA\n";
-}
+// Location::Location(
+// 	const std::string	path, \
+// 	const std::string	root, \
+// 	const bool			GET, \
+// 	const bool			POST, \
+// 	const bool			DELETE, \
+// 	const bool			autoindex, \
+// 	std::string			redirection, \
+// 	std::string			destination 
+// ) : path(path), root(root), GET(GET), POST(POST), DELETE(DELETE), autoindex(autoindex), redirection(redirection), destination(destination)
+// {
+// 	if (GET && POST && DELETE && autoindex)
+// 		std::cout << "HOLA\n";
+// }
 
+Location::Location(\
+				const std::string	&path, \
+				const std::string	&root, \
+				const std::string	&index, \
+				const std::string	&allowed_methods, \
+				const std::string	&autoindex, \
+				const std::string	&max_body_size, \
+				const std::string	&redirection, \
+				const std::string	&destination, \
+				const std::string	&cgi_destinaation \
+) :
+path(path),
+root(root),
+index(index),
+allowed_methods(allowed_methods),
+GET(isAllowed("GET")),
+POST(isAllowed("POST")),
+DELETE(isAllowed("DELETE")),
+autoindex(autoindex.find("on") == 0),///hmmmm
+max_body_size(atol(max_body_size.c_str())),
+redirection(redirection),
+destination(destination),
+cgi_destinaation(cgi_destinaation)
+{
+
+}
 /* Location::Location(const Location& toCopy) */
 /* { */
 /* 	(void)toCopy; */
@@ -34,4 +60,9 @@ Location& Location::operator=(const Location& toAssign)
 {
 	(void)toAssign;
 	return *this;
+}
+
+bool	Location::isAllowed(const std::string	&method)
+{
+	return(allowed_methods.find(method) != std::string::npos);
 }
--- srcs/classes/Location.hpp
+++ srcs/classes/Location.hpp
@@ -6,33 +6,108 @@
 
 class Location
 {
-	private:
+	private://mejor const
 		std::string path;
 		std::string	root;
-		const bool		GET;
-		const bool		POST;
-		const bool		DELETE;
-		const bool		autoindex;
+		std::string	index;
+		std::string	allowed_methods;
+		bool	GET;
+		bool	POST;
+		bool	DELETE;
+		bool	autoindex;
+		size_t		max_body_size;
 		std::string	redirection;
 		std::string	destination;
+		std::string	cgi_destinaation;
 
+		Location();
+		bool	isAllowed(const std::string	&method);
 
 	public:
-		Location();
 		Location(\
 				const std::string	path, \
 				const std::string	root, \
+				const std::string	index, \
 				const bool			GET, \
 				const bool			POST, \
 				const bool			DELETE, \
 				const bool			autoindex, \
+				size_t				max_body_size,\
 				std::string			redirection, \
-				std::string			destination \
+				std::string			destination, \
+				std::string			cgi_destinaation \
+		);
+		Location(\
+				const std::string	&path, \
+				const std::string	&root, \
+				const std::string	&index, \
+				const std::string	&allowed_methods, \
+				const std::string	&autoindex, \
+				const std::string	&max_body_size, \
+				const std::string	&redirection, \
+				const std::string	&destination, \
+				const std::string	&cgi_destinaation \
 		);
 		/* Location(const Location& toCopy); */
 		~Location();
 
 		Location& operator=(const Location& toCopy);
+
+	 const std::string& getPath() const
+    {
+        return path;
+    }
+
+    const std::string& getRoot() const
+    {
+        return root;
+    }
+
+    const std::string& getIndex() const
+    {
+        return index;
+    }
+
+    bool isGET() const
+    {
+        return GET;
+    }
+
+    bool isPOST() const
+    {
+        return POST;
+    }
+
+    bool isDELETE() const
+    {
+        return DELETE;
+    }
+
+    bool isAutoindex() const
+    {
+        return autoindex;
+    }
+
+    size_t getMaxBodySize() const
+    {
+        return max_body_size;
+    }
+
+    const std::string& getRedirection() const
+    {
+        return redirection;
+    }
+
+    const std::string& getDestination() const
+    {
+        return destination;
+    }
+
+    const std::string& getCGIDestination() const
+    {
+        return cgi_destinaation;
+    }
+
 };
 
 #endif
