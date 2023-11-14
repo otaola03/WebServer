97d4cabf172d11b645def915c73378e315324079
Xabier Matute Rusinol <xmatute-@c3r7s3.42urduliz.com>
2023-11-14 18:11:57 +0100

    srry

--- srcs/classes/WebServer.hpp
+++ srcs/classes/WebServer.hpp
@@ -3,7 +3,6 @@
 # define WEBSERVER_HPP
 
 #include "Server.hpp"
-#include "Config.hpp"
 
 typedef std::vector<Server*> serverVector;
 typedef std::map<int, Port*> intPortMap;
@@ -11,15 +10,13 @@ typedef std::map<int, Port*> intPortMap;
 class WebServer
 {
 	private:
-		fd_set			portsList;
-		fd_set			socketList;
+		fd_set	portsList;
+		fd_set	socketList;
 		serverVector	serversList;
 
-		WebServer();
-
 	public:
+		WebServer();
 		WebServer(const WebServer& toCopy);
-		WebServer(const Config& config);
 		~WebServer();
 
 		void	serverLoop();
